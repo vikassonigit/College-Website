f07a6512f3d1b792b51a50da33aa6b385e228774
Jaspreet Singh Kohli <70095662+Jaspreetkohli72@users.noreply.github.com>
2021-10-21 09:55:55 +0530

    feature added - nav bar changes color on scroll

--- js/progressBar.js
+++ js/progressBar.js
@@ -1,7 +1,17 @@
 let progress = document.getElementById("progressBar");
 let totalHeight = document.body.scrollHeight - window.innerHeight;
+const navbar = document.querySelector(".nav"); //Declaring variable for nav bar
 
 window.onscroll = function () {
 	let progressHeight = (window.pageYOffset / totalHeight) * 100;
 	progress.style.height = progressHeight + "%";
+
+	var top = window.scrollY; //getting position
+	if (top >= 600) {
+		//if position is >= 600 pixel active class is aaded
+		navbar.classList.add("active");
+	} else {
+		//else active class is removed
+		navbar.classList.remove("active");
+	}
 };
